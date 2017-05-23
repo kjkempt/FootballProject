4be855b657ddc7db3b0c35c32f2304cafd477ae5
Blake Skaja <bskaja@iastate.edu>
2017-05-23 07:46:20 -0700

    A little less shitty login system.

--- routes/index.js
+++ routes/index.js
@@ -20,29 +20,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-function validateLogin(username, password) {
-    console.log(username);
-    console.log(password);
-    var sql = "SELECT username, password FROM testUser WHERE username= " + "'" + username + "'";
-
-    return connection.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log(result);
-        if (result.length == 0) {
-            return false;
-        }
-
-        return true;
-    });
-}
-
 router.post('/login', function(req, res) {
-  console.log(req.body);
-  if (validateLogin(req.body.username, req.body.password)) {
-      res.render('index', { title: 'Logged In' });
-  } else {
-      res.render('index', { title: 'Failed' });
-  }
+
+  var sql = "SELECT username, password FROM testUser WHERE username= " + "'" + req.body.username + "'";
+    
+  connection.query(sql, function (err, result) {
+      if (err) throw err;
+
+      if (result.length !== 0 && result[0].password === req.body.password) {
+          res.render('index', { title: 'Logged In' });
+      } else {
+          res.render('index', { title: 'Failed' });
+      }
+  });
 });
 
 module.exports = router;
