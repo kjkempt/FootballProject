4e279411b17f7db5defaf8ec5cc61ea5b8d0bcf4
Kyle Kempt <kyle.kempt@gmail.com>
2017-05-24 00:05:40 -0500

    WM page updated w/ basic dropdown RPE select form and date

--- routes/workoutManager.js
+++ routes/workoutManager.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'footballdb.cr1jtswtem4i.us-west-2.rds.amazonaws.com',
+    user     : 'masterUsername',
+    password : 'HNuxJSEjqXUS!auk-eRV6CG8+!^JJAt2M?-Lc4y#+',
+    database : 'master'
+});
+
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+
+
 /* GET home page. Template for if you need to add an custom routers for this.*/
 router.get('/', function(req, res, next) {
     res.render('workoutManager', { title: 'Express' });
