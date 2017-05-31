e66ec2c6c4a414c310877c7e0d5e78e010c71638
Kyle Kempt <kyle.kempt@gmail.com>
2017-05-30 19:57:39 -0500

    Coach dashboard data tables attempt

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+
 var coachDashboard = require('./routes/coachDashboard');
 var playerDashboard = require('./routes/playerDashboard');
 var users = require('./routes/users');
