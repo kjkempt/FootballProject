062056b67fc73cd36160265c1306ead7c2191db7
Kyle Kempt <>
2018-04-20 01:45:59 -0500

    First changes in a while, adding more specific positions to sport, need to continue to branch out to multiple sports

--- routes/adminAddAthlete.js
+++ routes/adminAddAthlete.js
@@ -47,9 +47,9 @@ router.post('/addPerson', function(req, res, next) {
                 " '" + req.body.last_name + "', 'CO', '"+teamid[0].teamID+"');";
         }
         else {
-            sql = "INSERT INTO master.user (username, password, privileges, first_name, last_name, position, teamID, meal) " +
+            sql = "INSERT INTO master.user (username, password, privileges, first_name, last_name, position, teamID) " +
                 "VALUES('" + req.body.user_name + "', '" + req.body.password + "', '" + req.body.priv + "', '" + req.body.first_name + "'," +
-                " '" + req.body.last_name + "', '" + req.body.position + "', '"+teamid[0].teamID+"', '"+ req.body.meal +"');";
+                " '" + req.body.last_name + "', '" + req.body.position + "', '"+teamid[0].teamID+"');";
         }
 
 
