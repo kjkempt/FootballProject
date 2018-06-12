438d2ceaba04bacf4f4db03237606d40a52dd0cd
Kyle Kempt <>
2018-06-12 10:26:02 -0500

    Patch Fixed update player data so it allows one or both columns to be filled not just both

--- routes/isuwbb/isuwbbUpdatePlayerData.js
+++ routes/isuwbb/isuwbbUpdatePlayerData.js
@@ -33,84 +33,247 @@ router.post('/update', function(req, res, next) {
 
             updateWork = result;
 
+            if(!req.body.minutes)
+            {
+                sql = "UPDATE master.player_workouts " +
+                    "SET heartrate = " + req.body.heartrate + " " +
+                    "WHERE id = '"+ updateWork[0].id +"'; ";
 
 
 
-        sql = "UPDATE master.player_workouts " +
-            "SET ind_duration = " + req.body.minutes + ", " +
-            "heartrate = " + req.body.heartrate + " " +
-            "WHERE id = '"+ updateWork[0].id +"'; ";
-
-
-        connection.query(sql, function (err, result) {
-            if (err) throw err;
-
-
-            sql = "SELECT * FROM workouts " +
-                "WHERE teamID = '" + teamid[0].teamID + "' " +
-                "ORDER BY date DESC LIMIT 1;";
-
-            var recent_dates = [];
-            connection.query(sql, function (err, result) {
-                if (err) throw err;
-
-                recent_dates = result;
-
-                for (var i = 0; i < recent_dates.length; i++) {
-
-                    var day = recent_dates[i].date;
-
-                    day = day.toISOString().split('T')[0];
-
-                    recent_dates[i].date = day;
-
-                    recent_dates[i].date = recent_dates[i].date + " " + recent_dates[i].time + ", " + recent_dates[i].name;
-
-
-                }
-
-
-                sql = "SELECT * FROM master.workouts " +
-                    "WHERE teamID = '" + teamid[0].teamID + "' " +
-                    "AND workoutid = " +
-                    "(select max(workoutid) from master.workouts " +
-                    "WHERE teamID = '" + teamid[0].teamID + "')  ;";
-
-
-                var workoutid = [];
                 connection.query(sql, function (err, result) {
-
-                    workoutid = result;
-
-                    sql = "SELECT u.first_name, u.last_name, u.username " +
-                        "FROM master.player_workouts p, master.user u " +
-                        "where p.workoutID = '" + workoutid[0].workoutid + "' AND " +
-                        "u.username = p.username;";
+                    if (err) throw err;
 
 
-                    var players = [];
+                    sql = "SELECT * FROM workouts " +
+                        "WHERE teamID = '" + teamid[0].teamID + "' " +
+                        "ORDER BY date DESC LIMIT 1;";
+
+                    var recent_dates = [];
                     connection.query(sql, function (err, result) {
+                        if (err) throw err;
 
-                        players = result;
+                        recent_dates = result;
+
+                        for (var i = 0; i < recent_dates.length; i++) {
+
+                            var day = recent_dates[i].date;
+
+                            day = day.toISOString().split('T')[0];
+
+                            recent_dates[i].date = day;
+
+                            recent_dates[i].date = recent_dates[i].date + " " + recent_dates[i].time + ", " + recent_dates[i].name;
 
 
-                        res.render('isuwbb/isuwbbUpdatePlayerData', {
-                            username: req.session.user,
-                            recent: recent_dates,
-                            message: "Success",
-                            players: players
+                        }
+
+
+                        sql = "SELECT * FROM master.workouts " +
+                            "WHERE teamID = '" + teamid[0].teamID + "' " +
+                            "AND workoutid = " +
+                            "(select max(workoutid) from master.workouts " +
+                            "WHERE teamID = '" + teamid[0].teamID + "')  ;";
+
+
+                        var workoutid = [];
+                        connection.query(sql, function (err, result) {
+
+                            workoutid = result;
+
+                            sql = "SELECT u.first_name, u.last_name, u.username " +
+                                "FROM master.player_workouts p, master.user u " +
+                                "where p.workoutID = '" + workoutid[0].workoutid + "' AND " +
+                                "u.username = p.username;";
+
+
+                            var players = [];
+                            connection.query(sql, function (err, result) {
+
+                                players = result;
+
+
+                                res.render('isuwbb/isuwbbUpdatePlayerData', {
+                                    username: req.session.user,
+                                    recent: recent_dates,
+                                    message: "Success",
+                                    players: players
+                                });
+
+                            });
+
+
                         });
+
+
+                    });
+
+
+                });
+            }
+            else if(!req.body.heartrate)
+            {
+                sql = "UPDATE master.player_workouts " +
+                    "SET ind_duration = " + req.body.minutes + " " +
+                    "WHERE id = '"+ updateWork[0].id +"'; ";
+
+
+
+                connection.query(sql, function (err, result) {
+                    if (err) throw err;
+
+
+                    sql = "SELECT * FROM workouts " +
+                        "WHERE teamID = '" + teamid[0].teamID + "' " +
+                        "ORDER BY date DESC LIMIT 1;";
+
+                    var recent_dates = [];
+                    connection.query(sql, function (err, result) {
+                        if (err) throw err;
+
+                        recent_dates = result;
+
+                        for (var i = 0; i < recent_dates.length; i++) {
+
+                            var day = recent_dates[i].date;
+
+                            day = day.toISOString().split('T')[0];
+
+                            recent_dates[i].date = day;
+
+                            recent_dates[i].date = recent_dates[i].date + " " + recent_dates[i].time + ", " + recent_dates[i].name;
+
+
+                        }
+
+
+                        sql = "SELECT * FROM master.workouts " +
+                            "WHERE teamID = '" + teamid[0].teamID + "' " +
+                            "AND workoutid = " +
+                            "(select max(workoutid) from master.workouts " +
+                            "WHERE teamID = '" + teamid[0].teamID + "')  ;";
+
+
+                        var workoutid = [];
+                        connection.query(sql, function (err, result) {
+
+                            workoutid = result;
+
+                            sql = "SELECT u.first_name, u.last_name, u.username " +
+                                "FROM master.player_workouts p, master.user u " +
+                                "where p.workoutID = '" + workoutid[0].workoutid + "' AND " +
+                                "u.username = p.username;";
+
+
+                            var players = [];
+                            connection.query(sql, function (err, result) {
+
+                                players = result;
+
+
+                                res.render('isuwbb/isuwbbUpdatePlayerData', {
+                                    username: req.session.user,
+                                    recent: recent_dates,
+                                    message: "Success",
+                                    players: players
+                                });
+
+                            });
+
+
+                        });
+
 
                     });
 
 
                 });
 
+            }
+            else
+            {
+                sql = "UPDATE master.player_workouts " +
+                    "SET ind_duration = " + req.body.minutes + ", " +
+                    "heartrate = " + req.body.heartrate + " " +
+                    "WHERE id = '"+ updateWork[0].id +"'; ";
 
-            });
 
 
-        });
+                connection.query(sql, function (err, result) {
+                    if (err) throw err;
+
+
+                    sql = "SELECT * FROM workouts " +
+                        "WHERE teamID = '" + teamid[0].teamID + "' " +
+                        "ORDER BY date DESC LIMIT 1;";
+
+                    var recent_dates = [];
+                    connection.query(sql, function (err, result) {
+                        if (err) throw err;
+
+                        recent_dates = result;
+
+                        for (var i = 0; i < recent_dates.length; i++) {
+
+                            var day = recent_dates[i].date;
+
+                            day = day.toISOString().split('T')[0];
+
+                            recent_dates[i].date = day;
+
+                            recent_dates[i].date = recent_dates[i].date + " " + recent_dates[i].time + ", " + recent_dates[i].name;
+
+
+                        }
+
+
+                        sql = "SELECT * FROM master.workouts " +
+                            "WHERE teamID = '" + teamid[0].teamID + "' " +
+                            "AND workoutid = " +
+                            "(select max(workoutid) from master.workouts " +
+                            "WHERE teamID = '" + teamid[0].teamID + "')  ;";
+
+
+                        var workoutid = [];
+                        connection.query(sql, function (err, result) {
+
+                            workoutid = result;
+
+                            sql = "SELECT u.first_name, u.last_name, u.username " +
+                                "FROM master.player_workouts p, master.user u " +
+                                "where p.workoutID = '" + workoutid[0].workoutid + "' AND " +
+                                "u.username = p.username;";
+
+
+                            var players = [];
+                            connection.query(sql, function (err, result) {
+
+                                players = result;
+
+
+                                res.render('isuwbb/isuwbbUpdatePlayerData', {
+                                    username: req.session.user,
+                                    recent: recent_dates,
+                                    message: "Success",
+                                    players: players
+                                });
+
+                            });
+
+
+                        });
+
+
+                    });
+
+
+                });
+
+            }
+
+
+
+
 
         });
 
